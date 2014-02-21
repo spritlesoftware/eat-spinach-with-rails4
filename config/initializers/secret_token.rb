092b91771d9e79174cb2479efdc76fe2f449b170
Balaji D Loganathan <info@spritle.com>
2014-02-21 14:45:44 +0530

    adding secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change t  his key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EatSpinachWithRails::Application.config.secret_key_base = 'f0420e1a3594b4fdfd01b52b159195d7318a77ffb6ddc16faa0c6cb205d0372bad9e466b2265504443ca9077884cd3916360e2fc69b58d97c918365b4830dc83'
